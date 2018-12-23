f4188c018bc16e0bdf3b50d3a97f898f65fbd7a8
dashanhust <liujinshan.2008.08@163.com>
2018-12-23 17:27:08 +0800

    edit localStorage.js 空格

--- localStorageOpt.js
+++ localStorageOpt.js
@@ -3,32 +3,32 @@
  */
 
 let localStorageOpt = {
-	expire:  1000 * 60 * 60 * 24,  // default one day expire time
-	setValue: function (key, value) {
-		let data = JSON.stringify({
-			time: new Date().getTime(),
-			data: value
-		});
-		window.localStorage.setItem(key, data);
-	},
-	getValue: function (key, exp=this.expire) {
-		let data = window.localStorage.getItem(key);
-		
-		if (data == null) {
-			return {result: false, message: 'key not exist'};
-		}
-		
-		let dataObj = JSON.parse(data);
-		if (new Date().getTime() - dataObj.time > this.expire) {
-			return {result: false, messge: 'key expired'};
-		} else {
-			return {result: true, data: JSON.parse(dataObj.data), message: 'get data from localStorage success'};
-		}		
-	},
-	delValue: function (key) {
-		window.localStorage.removeItem(key);
-	},
-	clearStorage: function () {
-		window.localStorage.clear();
-	}
+    expire:  1000 * 60 * 60 * 24,  // default one day expire time
+    setValue: function (key, value) {
+        let data = JSON.stringify({
+            time: new Date().getTime(),
+            data: value
+        });
+        window.localStorage.setItem(key, data);
+    },
+    getValue: function (key, exp=this.expire) {
+        let data = window.localStorage.getItem(key);
+        
+        if (data == null) {
+            return {result: false, message: 'key not exist'};
+        }
+        
+        let dataObj = JSON.parse(data);
+        if (new Date().getTime() - dataObj.time > this.expire) {
+            return {result: false, messge: 'key expired'};
+        } else {
+            return {result: true, data: JSON.parse(dataObj.data), message: 'get data from localStorage success'};
+        }        
+    },
+    delValue: function (key) {
+        window.localStorage.removeItem(key);
+    },
+    clearStorage: function () {
+        window.localStorage.clear();
+    }
 }
