d577be73fdebbf064fff176a31dbc262d9541db7
dashanhust <297253634@qq.com>
2019-01-18 22:21:11 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -98,12 +98,12 @@ if (typeof Date.prototype.toTimeFormatString != 'function') {
 // 将unicode编码字符串转换为中文
 function reconvertFromUnicodeToChinese(str) {
     return str.replace(/(\\u)(\w{1,4})/gi, function ($0) {
-				return (String.fromCharCode(parseInt((escape($0).replace(/(%5Cu)(\w{1,4})/g, "$2")), 16)));
-			})
-			.replace(/(&#x)(\w{1,4});/gi, function ($0) {
-				return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, "$2"), 16));
-			})
-			.replace(/(&#)(\d{1,6});/gi, function ($0) {
-				return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, "$2")));
-			});
+			return (String.fromCharCode(parseInt((escape($0).replace(/(%5Cu)(\w{1,4})/g, "$2")), 16)));
+		})
+		.replace(/(&#x)(\w{1,4});/gi, function ($0) {
+			return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, "$2"), 16));
+		})
+		.replace(/(&#)(\d{1,6});/gi, function ($0) {
+			return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, "$2")));
+		});
 }
